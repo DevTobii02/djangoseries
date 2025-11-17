87742dc038a25958fd5a367068ad7c40c10a2375
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-11-17 09:04:14 +0100

    created projcts and apps

---
+++ djangoseries/profiles/models.py
@@ -0,0 +1,8 @@
+#This model.py file describes the database structure for anything related to products
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField
