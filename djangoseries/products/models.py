30d6f2cc8104d63019ac0fb3de8177699159970d
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-11-17 09:44:48 +0100

    creatd the model for products

--- djangoseries/products/models.py
+++ djangoseries/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField
+    supplier = models.TextField()
+    summary = models.TextField(default="Very Nice Product")
