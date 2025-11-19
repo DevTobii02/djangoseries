35546f6a6948104c7964d3c8060a7aeb6c49d3bd
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-11-19 23:48:21 +0100

    registered NewProducts model from products/models.py

--- djangoseries/products/admin.py
+++ djangoseries/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Products
+from .models import NewProducts
+admin.site.register(Products)
+#added an example of a real data value
+admin.site.register(NewProducts)
