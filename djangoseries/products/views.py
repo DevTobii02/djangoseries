281ec969d3a2bc8135f2ae4f4e14f3ac7a79cf8a
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-11-26 16:32:01 +0100

    changed

--- djangoseries/products/views.py
+++ djangoseries/products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from  django.http import  HttpResponse
 # Create your views here.
+# def homepage(request):
+#     return render(request, 'homepage.html')
+# def about(request):
+#     return render(request, 'about.html')
+# def contact(request):
+#     return render(request, 'contact.html')
+# def products(request):
+#     return render(request, 'products.html')
+# def search(request):
+#     return render(request, 'search.html')
+def homepage_view(args, kwargs):
+    return HttpResponse ("<h1>Hello World!</h1>")
